304c1e899cdc78a8e233e0003fd2af82daf3e371
RAHATSHIRIN <kr8088703@gmail.com>
2024-10-15 02:44:59 +0600

    Update pv4.js

--- Priyansh/commands/pv4.js
+++ Priyansh/commands/pv4.js
@@ -6,7 +6,7 @@
 module.exports.config = {
     name: "pv4",
     version: "1.0.0",
-    hasPermission: 0,
+    hasPermission: 2,
     credits: "Mohammad Nayan",
     description: "Pair Mention Fun🥹",
     usePrefix: true,
