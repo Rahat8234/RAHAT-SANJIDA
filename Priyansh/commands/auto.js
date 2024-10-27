7a300932a9b8524f873940de716af9cecf83d6bd
RAHATSHIRIN <kr8088703@gmail.com>
2024-10-28 04:08:59 +0600

    Create auto.js

---
+++ Priyansh/commands/auto.js
@@ -0,0 +1,38 @@
+module.exports.config = {
+    name: "auto",
+    version: "1.0.0",
+    hasPermission: 0,
+    credits: "RAHAT",
+    description: "all video download",
+    usePrefix: true,
+    commandCategory: "Khan Rahul RK",
+    usages: "All Video download",
+    cooldowns: 5,
+},
+start: async function({ nayan, events, args }) {},
+handleEvent: async function ({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const {alldown} = require("nayan-media-downloader")
+  if (body.startsWith("https://")) {
+  api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
+const data = await alldown(content);
+  console.log(data)
+  const {low, high, title} = data.data;
+    api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
+  const video = (await axios.get(high, {
+      responseType: "arraybuffer",
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
+
+        return api.sendMessage({
+            body: `《TITLE》: ${title}`,
+            attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
+
+        }, event.threadID, event.messageID);
+    }
+}
+}
